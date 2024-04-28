891a37f656605fd50802742e8c0ecb658f14b08d
Balaji Rajaram <51748359+Balaji-Rajaram@users.noreply.github.com>
2024-04-28 12:04:13 +0530

    first update

--- script/index.js
+++ script/index.js
@@ -25,14 +25,17 @@ function renderProject(data) {
         })
         
         HTMLString += `<div class="w-full bg-lightNavy shadow-md p-5 rounded-md">
-        <div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-8 h-8 text-primary">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
-            </svg>
+        <div class="w-full flex justify-between">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-8 h-8 text-primary">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+                        </svg>
+                        <a href="${element.url}" target="_blank">
+                            <i class="fa-brands fa-github text-primary text-lg hover:-translate-y-1 hover:scale-110 delay-150 transition ease-in-out"></i>
+                        </a>
 
-        </div>
+                    </div>
         <p class="font-semibold text-lightSlate text-2xl mt-3">${element.title}
         </p>
         <p class="text-slate mt-3 ">${element.description}</p>
@@ -86,4 +89,19 @@ function selectedTag(data) {
         prevSelectedElement.classList.remove("border-primary")
     }
     prevSelectedTag = data
+}
+
+function navTo(data) {
+    if(data == "about"){
+        document.getElementById('about').scrollIntoView()
+    }
+    if(data == "experience"){
+        document.getElementById('experience').scrollIntoView()
+    }
+    if(data == "project"){
+        document.getElementById('project').scrollIntoView()
+    }
+    if(data == "contact"){
+        document.getElementById('contact').scrollIntoView()
+    }
 }
---
+++ script/loader.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#loadIcon").css("transform", "scale(1.2)"); // Fade in the element over 1 second (1000 milliseconds)
+});
+
+setTimeout(() => {
+   document.getElementById("loading").style.display = "none"
+   document.getElementById("bodyContent").hidden = false
+   document.getElementById("bodyContent").classList.add("animate-fadeIn")
+
+}, 100);
+
